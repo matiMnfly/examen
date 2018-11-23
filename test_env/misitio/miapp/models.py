d9dba4bee41751625a716ee066b0e3b6a61220e9
matiMnfly <llamamek@gmail.com>
2018-11-23 14:16:51 -0300

    comiendo

---
+++ test_env/misitio/miapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Producto(models.Model):
+    Nombre = models.CharField( max_length=200, null=None)
+    Costo_presupuestado = models.BigIntegerField()
+    Costo_real = models.BigIntegerField()
+    Tienda = models.CharField(max_length=200)
+    Notas = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.Nombre
