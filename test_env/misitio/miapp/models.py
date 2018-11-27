ea2119b18b973f06e9ed4a46621fda99ade86e80
matiMnfly <llamamek@gmail.com>
2018-11-27 12:55:44 -0300

    gg

--- test_env/misitio/miapp/models.py
+++ test_env/misitio/miapp/models.py
@@ -4,7 +4,6 @@ from django.utils import timezone
 # Create your models here.
 
 class Producto(models.Model):
-    Usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     Nombre = models.CharField( max_length=200, null=None)
     Costo_presupuestado = models.BigIntegerField()
     Costo_real = models.BigIntegerField()
